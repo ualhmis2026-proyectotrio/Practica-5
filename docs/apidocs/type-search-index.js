8e9bc636a0f283309ded383ad2ccdb7617b556dd
David Ureña Arroniz <bluberyasp161@gmail.com>
2026-04-11 14:02:24 +0200

    docs con diagrama de clases y pon actualizado con todos los plugins

---
+++ docs/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.hmis","l":"Coche"},{"p":"org.hmis","l":"JsonReader"},{"p":"org.hmis","l":"Main"}];updateSearchResults();
